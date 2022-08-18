f8cff4615b0c3f7133335c6f7ed89c3e439791d1
Marissa Tobin <marissatobin@192-168-1-100.tpgi.com.au>
2022-08-18 19:16:25 +0800

    fix database

--- db/schema.sql
+++ db/schema.sql
@@ -6,19 +6,19 @@ USE fuel_db;
 CREATE TABLE `fuel_db`.`user` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(255) NOT NULL,
-  `post code` CHAR(4) NOT NULL,
+  `post code` INT NOT NULL,
   `search radius` INT NOT NULL,
-  `fuel type` CHAR(3) NOT NULL,
+  `fuel type` INT NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
   UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE);
 
-  CREATE TABLE `fuel_db`.`servo` (
-  `company name` VARCHAR(255) NOT NULL,
-  `address` VARCHAR(45) NOT NULL,
+
+CREATE TABLE `fuel_db`.`servo` (
   `id` INT NOT NULL,
   `user_id` INT NOT NULL,
-  `price` DECIMAL(3) NOT NULL,
-  `distance` DECIMAL(2) NOT NULL,
-  UNIQUE INDEX `address_UNIQUE` (`address` ASC) VISIBLE,
-  PRIMARY KEY (`user_id`));
+  `address` VARCHAR(255) NOT NULL,
+  `company name` VARCHAR(25) NOT NULL,
+  `price` DECIMAL(5,2) NOT NULL,
+  `distance` INT NOT NULL,
+  PRIMARY KEY (`id`));
