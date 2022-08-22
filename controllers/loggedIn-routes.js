a7a2eb16a70b350e71ce4c2d2ff6e2af3ed02b23
Ethan Godley <62864699+ethangodley@users.noreply.github.com>
2022-08-22 19:18:53 +0800

    Added index.js to models, fixed file paths

--- controllers/loggedIn-routes.js
+++ controllers/loggedIn-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const path = require('path');
-const { User } = require('../models');
+const { User } = require('../models/user');
 
 // This is the 'get' route 
 router.get('/', async (req, res) => {
