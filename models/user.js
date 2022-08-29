7aaabe27dd465a76c2b19c1427e13aa3296c1713
Yoko-cyer <yokoujihara622@gmail.com>
2022-08-29 14:55:23 +1000

    first commit

--- models/user.js
+++ models/user.js
@@ -8,55 +8,6 @@ class User extends Model {
   }
 }
 
-<<<<<<< HEAD
- User.init(
-     {
-         id: {
-             type: DataTypes.INTEGER,
-             allowNull: false,
-             primaryKey: true,
-             autoIncrement: true,
-         },
-         username: {
-             type: DataTypes.STRING,
-             allowNull: false,
-         },
-         password: {
-            type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-               len: [8],
-           },
-          },
-         surroundingSuburbs: {
-             type: DataTypes.STRING,
-             allowNull: false,
-         },
-         fuelType: {
-             type: DataTypes.STRING,
-             allowNull: false,
-
-         },
-     },
-     {
-         hooks: {
-             beforeCreate: async (newUserData) => {
-                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                 return newUserData;
-             },
-             beforeUpdate: async (updatedUserData) => {
-                 updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-                 return updatedUserData;
-             },
-         },
-         sequelize,
-         timestamps: false,
-         freezeTableName: true,
-         underscored: true,
-         modelName: 'user',
-     }
- );
-=======
 User.init(
   {
     id: {
@@ -103,6 +54,5 @@ User.init(
     modelName: 'user',
   }
 );
->>>>>>> d41f6d6911b07094a54d1d68f2d6322f38663aef
 
 module.exports = User;
